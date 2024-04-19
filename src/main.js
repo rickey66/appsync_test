00328f03eb6915141e97321202d6b6fb15990833
RyuichiNakata <rnakata@biz-d.email>
2024-04-19 12:17:06 +0900

    appsyncの接続情報を隠蔽

--- src/main.js
+++ src/main.js
@@ -14,10 +14,10 @@ import { Amplify } from 'aws-amplify';
 Amplify.configure({
     API: {
         GraphQL: {
-            endpoint: 'https://ez3vv72fy5dlbgg2oo6wcirwpy.appsync-api.ap-northeast-1.amazonaws.com/graphql',
+            endpoint: 'https://**********.appsync-api.ap-northeast-1.amazonaws.com/graphql',
             region: 'ap-northeast-1',
             defaultAuthMode: 'apiKey',
-            apiKey: 'da2-um5ba6hq6vemzhmqd4yqxj5vfu'
+            apiKey: '**********'
         }
     }
 });
